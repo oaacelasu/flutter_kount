b9341c3b9a27c94c0c336f37ea53aab51d03747e
Oscar Daza <odaza@paymentez.com>
2020-02-06 11:46:10 -0500

    sessionId IOS

--- ios/Classes/SwiftFlutterKountPlugin.swift
+++ ios/Classes/SwiftFlutterKountPlugin.swift
@@ -1,6 +1,9 @@
 import Flutter
 import UIKit
 
+
+//#import "KDataCollector.h"
+
 public class SwiftFlutterKountPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_kount", binaryMessenger: registrar.messenger())
@@ -9,6 +12,18 @@ public class SwiftFlutterKountPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    //result(UUID().uuidString.replacingOccurrences(of: "-", with: ""))
+
+   // let deviceCollector:KDataCollector = KDataCollector.shared()
+
+   // deviceCollector.environment = KEnvironment.test
+
+   //        let sessioniD = UUID().uuidString.replacingOccurrences(of: "-", with: "")
+
+   //         collect(sessioniD) { (error) in
+
+       //     }
+   //         result(sessioniD)
+
   }
 }
